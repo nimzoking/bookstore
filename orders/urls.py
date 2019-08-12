a731cb32d5337c64df57e76486e4192cdba86a1b
Carl Tillotson <nimzoking@gmail.com>
2019-08-12 21:27:26 +0100

    Chapter 14 - added .gitignore

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 # orders/urls.py
 from django.urls import path
 
-from .views import OrdersPageView
+from .views import OrdersPageView, charge
 
 urlpatterns = [
+    path('charge/', charge, name='charge'),
     path('', OrdersPageView.as_view(), name='orders'),
 ]
